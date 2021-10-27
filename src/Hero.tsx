df30e27e6309bf7ac48b15eb3ff1ce85711caefc
Kyle Geib <kyle.geib@gmail.com>
2021-10-27 16:43:07 -0400

    M3L6 - Improved responsiveness Don't know what's up with the split making the image disappear.

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Cover } from "@bedrock-layout/cover";
 import { Frame } from "@bedrock-layout/frame";
 import { Stack } from "@bedrock-layout/stack";
@@ -8,9 +9,18 @@ import { InlineCluster } from "@bedrock-layout/inline-cluster";
 import dataImage from "./images/data.jpg";
 
 export default function Hero() {
+  const menuRef = useRef<HTMLDivElement>(null);
+  const menuHeight = menuRef.current?.getBoundingClientRect().height ?? 48;
+
   return (
     <Stack as="header" gutter="sm">
-      <Inline gutter="lg" align="center" stretch={1}>
+      <Inline
+        gutter="lg"
+        align="center"
+        stretch={1}
+        ref={menuRef}
+        switchAt="40rem"
+      >
         <h1>Arrow Data</h1>
 
         {/* It's great to support accessibility with elements like nav.
@@ -24,15 +34,15 @@ export default function Hero() {
           </InlineCluster>
         </nav>
 
-        <InlineCluster gutter="lg" align="center">
+        <InlineCluster gutter="lg" align="center" justify="end">
           <a href="/#">Sign in</a>
           <button>Sign up</button>
         </InlineCluster>
       </Inline>
 
-      <Split gutter="sm">
-        {/* 48px is the height of the navbar. 0.25rem is its gutter. */}
-        <Cover gutter="none" minHeight={`calc(100vh - 48px - 0.25rem)`}>
+      <Split gutter="sm" switchAt="60rem">
+        {/* 0.25rem is the navbar's gutter */}
+        <Cover gutter="none" minHeight={`calc(100vh - ${menuHeight}px - 0.25rem)`}>
           <Stack as="section" gutter="xl">
             <h2>Data Solutions</h2>
             <p>
@@ -41,10 +51,10 @@ export default function Hero() {
               risus commodo viverra maecenas. Magna sit amet purus gravida quis
               blandit turpis cursus.
             </p>
-            <div>
+            <InlineCluster gutter="lg">
               <button>Get Started</button>
               <button>Live Demo</button>
-            </div>
+            </InlineCluster>
           </Stack>
         </Cover>
 
