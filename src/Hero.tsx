f8493a3d2252e5ea7d30dcb6942b1dd07a5d8fd3
Kyle Geib <kyle.geib@gmail.com>
2021-10-27 16:24:46 -0400

    M3L6 - Finished state

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,3 +1,5 @@
+import { Cover } from "@bedrock-layout/cover";
+import { Frame } from "@bedrock-layout/frame";
 import { Stack } from "@bedrock-layout/stack";
 import { Split } from "@bedrock-layout/split";
 import { Inline } from "@bedrock-layout/inline";
@@ -7,44 +9,49 @@ import dataImage from "./images/data.jpg";
 
 export default function Hero() {
   return (
-    <header>
-      <div>
+    <Stack as="header" gutter="sm">
+      <Inline gutter="lg" align="center" stretch={1}>
         <h1>Arrow Data</h1>
 
         {/* It's great to support accessibility with elements like nav.
             Don't just slap everything into divs. */}
         <nav>
-          <ul>
+          <InlineCluster as="ul" gutter="xl" justify="center">
             <li>Solutions</li>
             <li>Pricing</li>
             <li>Docs</li>
             <li>More</li>
-          </ul>
+          </InlineCluster>
         </nav>
 
-        <div>
+        <InlineCluster gutter="lg" align="center">
           <a href="/#">Sign in</a>
           <button>Sign up</button>
-        </div>
-      </div>
+        </InlineCluster>
+      </Inline>
 
-      <div>
-        <section>
-          <h2>Data Solutions</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc vel
-            risus commodo viverra maecenas. Magna sit amet purus gravida quis
-            blandit turpis cursus.
-          </p>
-          <div>
-            <button>Get Started</button>
-            <button>Live Demo</button>
-          </div>
-        </section>
+      <Split gutter="sm">
+        {/* 48px is the height of the navbar. 0.25rem is its gutter. */}
+        <Cover gutter="none" minHeight={`calc(100vh - 48px - 0.25rem)`}>
+          <Stack as="section" gutter="xl">
+            <h2>Data Solutions</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc vel
+              risus commodo viverra maecenas. Magna sit amet purus gravida quis
+              blandit turpis cursus.
+            </p>
+            <div>
+              <button>Get Started</button>
+              <button>Live Demo</button>
+            </div>
+          </Stack>
+        </Cover>
 
-        <img src={dataImage} alt="computer with graphs" />
-      </div>
-    </header>
+        <Frame position="70% 50%">
+          <img src={dataImage} alt="computer with graphs" />
+        </Frame>
+      </Split>
+    </Stack>
   );
 }
