366a1ddbb5e5c58b09b1289ca2e3011ec7045fe6
Kyle Geib <kyle.geib@gmail.com>
2021-10-28 10:57:23 -0400

    M4L2 - Showing how we can style an Inline with a new styled component... that is also a PadBox.

--- src/settingsPage/App.tsx
+++ src/settingsPage/App.tsx
@@ -1,20 +1,37 @@
+import { Inline } from "@bedrock-layout/inline";
+import { PadBox } from "@bedrock-layout/padbox";
+import styled from "styled-components";
+
+const MenuBar = styled(Inline)`
+  background: black;
+  color: white;
+  border-block-end: 1px solid #303030;
+`;
+
 export default function App() {
   return (
     <div>
-      <div>
+      {/* Stetching the nav ul with stretch={1}. */}
+      <MenuBar
+        as={PadBox}
+        padding={["md", "xl"]}
+        gutter="lg"
+        align="center"
+        stretch={1}
+      >
         <div>Logo</div>
         <nav>
-          <ul>
+          <Inline as="ul" gutter="sm">
             <li>Dashboard</li>
             <li>Job</li>
             <li>Applicants</li>
             <li>Company</li>
-          </ul>
+          </Inline>
         </nav>
         <input placeholder="Search" />
         <div>icon</div>
         <div>profile</div>
-      </div>
+      </MenuBar>
     </div>
   );
 }
