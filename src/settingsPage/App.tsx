a15913cc677049bc50a0a773c1f6b783102f1b8d
Kyle Geib <kyle.geib@gmail.com>
2021-10-28 12:49:51 -0400

    M4L3 - Basic rounded main

--- src/settingsPage/App.tsx
+++ src/settingsPage/App.tsx
@@ -1,13 +1,48 @@
+import styled from "styled-components";
+import { PadBox } from "@bedrock-layout/padbox";
+
 import { Menu } from "./Menu";
 
 export default function App() {
   return (
-    <div className="App">
-      {/* Stetching the nav ul with stretch={1}. */}
+    <div>
       <Menu />
-      <header>
-        <h1>Settings</h1>
-      </header>
+      <ContentArea>
+        <SettingsHeader>
+          <h1>Settings</h1>
+        </SettingsHeader>
+        <SettingsPane>
+          <nav>
+            <ul>
+              <li>Profile</li>
+              <li>Account</li>
+              <li>Password</li>
+              <li>Notifications</li>
+              <li>Billing</li>
+              <li>Integration</li>
+            </ul>
+          </nav>
+          <div>Placeholder for right hand side</div>
+        </SettingsPane>
+      </ContentArea>
     </div>
   );
 }
+
+const ContentArea = styled(PadBox).attrs(() => ({
+  padding: "xl",
+}))`
+  /**
+   * Learn about gradients here: https://developer.mozilla.org/en-US/docs/Web/CSS/gradient
+   */
+  background-image: linear-gradient(to bottom, black 14rem, whitesmoke 14rem);
+`;
+
+const SettingsHeader = styled.header`
+  color: white;
+`;
+
+const SettingsPane = styled.main`
+  background: white;
+  border-radius: 0.5rem;
+`;
